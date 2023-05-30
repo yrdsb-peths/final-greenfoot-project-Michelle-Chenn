71bae41b1a741e04025f6092d3d0465c1852bdb1
Michelle-Chenn <131167229+Michelle-Chenn@users.noreply.github.com>
2023-05-30 09:11:48 -0400

    Added Turtle Class

---
+++ Turtle.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Turtle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Turtle extends Actor
+{
+    GreenfootImage idle = new GreenfootImage("images/turtle_idle/tile000.png");
+    
+    /**
+     * Constructor
+     */
+    
+    public Turtle(){
+        setImage(idle);
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
