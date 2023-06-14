d6bb1a190cdeb243797389a242fe4db105439cf5
Michelle-Chenn <131167229+Michelle-Chenn@users.noreply.github.com>
2023-06-14 08:29:07 -0400

    Added titlescreen and start button

---
+++ TitleScreen.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Fish Blaster", 100);
+    Start start = new Start();
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, 150);
+        addObject(start, getWidth()/2, 250);
+    }
+    /**
+     * The main world act loop
+     */
+    public void act(){
+        //Start game if user clicks mouse
+        if(Greenfoot.mousePressed(start)){
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+}
